3794e75cf1faa1741762ae433777a397995df8f3
asharma414 <abhi.sharm.414@gmail.com>
2020-04-23 18:03:46 -0400

    first commit

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,21 @@
 class Transfer
-  # your code here
+  attr_reader :sender, :receiver
+  attr_accessor :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = 'pending'
+  end
+
+  def valid?
+    self.sender.valid? && self.receiver.valid? 
+  end
+
+  def execute_transaction
+    self.sender.deposit(-self.amount)
+    self.receiver.deposit(self.amount)
+    self.status = 'complete'
+  end
 end
