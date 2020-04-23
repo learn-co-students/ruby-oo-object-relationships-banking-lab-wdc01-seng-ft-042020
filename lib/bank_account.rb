f88fba22e8661d38f98d013b93c6b11b18920fdf
agdsarge <agdsargeant@gmail.com>
2020-04-23 14:44:21 -0400

    first commit

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,20 @@
 class BankAccount
+# can initialize a Bank Account (FAILED - 1)
+#     initializes with a name (FAILED - 2)
+#     always initializes with balance of 1000 (FAILED - 3)
+#     always initializes with a status of 'open' (FAILED - 4)
+#     can't change its name (FAILED - 5)
+    attr_reader :name
+    attr_accessor :balance, :status
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = 'open'
+    end
+
+    def deposit(cash)
+        self.balance += cash
+    end
 
 end
