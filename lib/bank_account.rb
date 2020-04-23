8ea4fbd4a7556650bb5aabcbb284ce8e2595a730
jlangley3 <jlangleyiii@aol.com>
2020-04-23 15:56:49 -0400

    almost there

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,11 +2,12 @@ class BankAccount
 
     attr_reader :name, :balance, :status
     attr_writer :balance, :status
-
+@@all = []
     def initialize(name)
         @name = name
         @balance = 1000
         @status = "open"
+        @@all << self
     end
 
     def deposit(money)
@@ -24,5 +25,7 @@ class BankAccount
     def close_account
         self.status = "closed"
     end
-
+    def self.all
+        @@all
+    end    
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,34 @@
+
+require 'pry'
+require_relative './bank_account.rb'
 class Transfer
-  # your code here
+   attr_reader :receiver, :sender, :amount, :status
+  #   attr_writer #:status
+
+    def initialize(sender, receiver, amount)
+         @sender = sender
+         @receiver = receiver
+         @status = "pending"
+         @amount = amount
+    end
+
+    def valid?
+    sender_account = sender.valid?
+    receiver_account = receiver.valid?
+    sender_account && receiver_account
+    end
+
+    def execute_transaction
+      if valid? 
+        sender.balance -= self.amount
+        receiver.balance += self.amount
+        @status = "complete"
+      else
+        @status = "rejected"
+      end
+      
+    end
+    
+     
+    
 end
