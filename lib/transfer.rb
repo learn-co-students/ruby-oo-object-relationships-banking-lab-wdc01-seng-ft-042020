b61a9ae86090c92852fad842b77278916ead244a
epittab <ep7m@me.com>
2020-04-23 18:13:09 -0400

    20 examples, 1 failure

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,34 +1,51 @@
 
-require 'pry'
-require_relative './bank_account.rb'
 class Transfer
    attr_reader :receiver, :sender, :amount, :status
-  #   attr_writer #:status
+   attr_writer :status
+
+
+  @@executed = []
 
     def initialize(sender, receiver, amount)
          @sender = sender
          @receiver = receiver
          @status = "pending"
          @amount = amount
+        
     end
 
     def valid?
-    sender_account = sender.valid?
-    receiver_account = receiver.valid?
-    sender_account && receiver_account
+      
+      self.sender.valid? && self.receiver.valid? && self.sender.balance > self.amount
+    
+    end
+
+    def check_unique?
+      !@@executed.include?(self)
     end
 
     def execute_transaction
-      if valid? 
-        sender.balance -= self.amount
-        receiver.balance += self.amount
-        @status = "complete"
+      if check_unique? && valid?
+        self.sender.balance -= self.amount
+        self.receiver.balance += self.amount
+        @@executed << self
+        self.status = "complete"
       else
-        @status = "rejected"
+        self.status = "rejected"
+        return "Transaction rejected. Please check your account balance."
+    
+        #"Transaction rejected. Please check your account balance."
       end
       
     end
+
+    def self.executed
+      @@executed
+    end
+
+    def reverse_transfer
+
+    end
     
-     
     
 end
